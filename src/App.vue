b509970aba7a749300b5f77de55db55292773dda
hedong <382130287@qq.com>
2018-12-14 18:34:01 +0800

    youhua

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,27 @@
   <div id="app">
     <drawer width="200px;" :show.sync="drawerShow" :show-mode="showMode" :drawer-style="{'background-color':'#35495e', width: '200px'}">
       <div slot="drawer">
-        aa
+      <div class="title">L.A.N</div>
+        
       </div>
       <!-- rourer-view 作为默认插槽内容 -->
-      <view-box ref="viewBox">
+      <!-- <view-box ref="viewBox" style='width: 100%;overflow-x: hidden;'>
         <div class='nav_bar' @click="drawerShow = !drawerShow">
           <x-icon type="navicon" size="35" style="fill:#fff;position:relative;"></x-icon>
         </div>
         <router-view></router-view>
-      </view-box>
+      </view-box> -->
+      <div style='width: 100%;height: 100%;overflow: scroll;'>
+        <div class='nav_bar' @click="drawerShow = !drawerShow">
+          <x-icon type="navicon" size="35" style="fill:#fff;position:relative;"></x-icon>
+        </div>
+        <router-view></router-view>
+      </div>
     </drawer>
   </div>
 </template>
 <script>
-import { Drawer,ViewBox } from 'vux'
+import { Drawer, ViewBox } from 'vux'
 export default {
   name: 'App',
   data() {
@@ -63,6 +70,10 @@ export default {
   left: 20px;
   width: 35px;
   height: 35px;
+}
+.title{
+  padding: 10px;
+  color: #fff
 }
 
 </style>
